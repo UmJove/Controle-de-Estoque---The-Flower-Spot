aa455c095ea7c4dce417a952828a3bac81af5b88
PedroVitor-cpu <pedrovitoralmeida10@gmail.com>
2025-08-11 21:11:46 -0300

    Class Flor

--- Produto.py
+++ Produto.py
@@ -1,3 +1,5 @@
+import time
+
 class Produto:
     def __init__ (self, id, nome, preco, quantidade):
         self.id = id
@@ -7,3 +9,27 @@ class Produto:
 
     def verificar_quantidade(self):
         return 
+
+class Flor(Produto):
+    def __init__(self, id, nome, preco, quantidade,tempo_vida,data_colheita):
+        super().__init__(id, nome, preco, quantidade)
+        self.tempo_vida=tempo_vida
+        self.data_colheita=data_colheita
+        
+
+    
+
+    def verificar_viabilidade_entrega(self,data_entrega):
+        colheita=time.strptime(self.data_colheita,"%d/%m/%Y")
+        colheita_segundos=time.mktime(colheita)
+        validade_segundos=colheita_segundos+((self.tempo_vida)*60*60*24)
+        entrega=time.strptime(data_entrega,"%d/%m/%Y")
+        entrega_segundos=time.mktime(entrega)
+        if entrega_segundos>validade_segundos:
+            print("Não é possivel fazer a entregar")
+        else:
+            dias=int((validade_segundos-entrega_segundos)/(60*60*24))
+            print(f"A entregar viavel dias {dias}")
+
+f1=Flor("Rosa Vermelha",10,5,1,10,"05/08/2025")
+f1.verificar_viabilidade_entrega("16/08/2025")
